cef19dc46ba818eb4801f7c48023b94464a0b1fb
yoneshinzato <chibariyo@gmail.com>
2020-08-01 19:45:10 -0300

    fim

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,26 @@
-const cep = document.querySelector("#cep")
+const cep = document.querySelector("#cep"); //1
+const showData = (result) => {
+  for (const campo in result) {
+    if (document.querySelector("#" + campo)) {
+      document.querySelector("#" + campo).value = result[campo];
+    }
+  }
+};
 
-cep.addEventListener('blur', (e)=>{
-    let search = cep.value.replace('-', '')
-    console.log(cep.value)
-})
+//2
+cep.addEventListener("blur", (event) => {
+  let search = cep.value.replace("-", "");
+  const options = {
+    method: "GET",
+    mode: "cors",
+    cache: "default",
+  };
+
+  fetch(`https://viacep.com.br/ws/${search}/json/`, options)
+    .then((oi) => {
+      oi.json().then((alo) => {
+        showData(alo);
+      });
+    })
+    .catch((event) => console.log("Deu erro " + event.message));
+});
